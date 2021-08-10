68eb11eb2e6d94aecf6bb6b045839ed1d29035fb
Semyen <sspukhov@yandex.ru>
2021-08-10 18:01:55 +0300

    fix parsing & number validation

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,41 @@ https.globalAgent.options.rejectUnauthorized = false;
 
 type ResponseType = any;
 
+function hasLinkTag(str: string): boolean {
+  return str.includes('</a>');
+}
+
+function isNumberCorrect(str: string): boolean {
+  const regexp = new RegExp('s*[3489]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*[0-9]s*');
+  return str.length === 10 && regexp.test(str);
+}
+
 export default function getOperatorNameByNumber(phoneNumber: string): Promise<string | null> {
   if (typeof phoneNumber !== 'string') {
-    throw new Error('GMON: phoneNumber format error. It should be passed as a string.');
+    throw new Error('GMON: phoneNumber format error. It should be passed as a string');
+  }
+
+  if (!isNumberCorrect(phoneNumber)) {
+    throw new Error('GMON: incorrect phone number');
   }
 
   return performRequest(phoneNumber).then(
     (response) => {
-      const regex = new RegExp(`<b>${phoneNumber}<\/b>(.*)<\/b>`);
+      const regex = new RegExp('(?<=Оператор: ).*?(?=<br>)');
       const match = response.match(regex);
 
-      if (match && match[1]) {
-        return match[1];
+      if (!match || !match[0]) {
+        return null;
       }
 
-      return null;
+      if (hasLinkTag(match[0])) {
+        const reg = new RegExp('(<([^>]+)>)', 'gi');
+        const operator = match[0].replace(reg, '');
+
+        return operator || null;
+      } else {
+        return match[0];
+      }
     },
     () => {
       return null;
